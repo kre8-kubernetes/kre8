d1fc39770a743b705b2f1155b637da05e9c31883
bradenaa <bradenaltstatt@gmail.com>
2019-03-02 20:08:13 -0800

    made a couple small style changes to forms and menu components

--- src/components/CreateMenuComponent.js
+++ src/components/CreateMenuComponent.js
@@ -3,9 +3,9 @@ import React from 'react';
 const CreateMenuComponent = (props) => {
   return (
     <div className='create_menu_component_container'>
-      <button id='pod' className='create_menu_component_container_form' onClick={props.handleMenuItemToShow}>Create a POD</button>
-      <button id='service' className='create_menu_component_container_form' onClick={props.handleMenuItemToShow}>Create a Service</button>
-      <button id='deployment' className='create_menu_component_container_form' onClick={props.handleMenuItemToShow}>Create a Deployment</button>
+      <button id='pod' className='create_menu_component_container_button' onClick={props.handleMenuItemToShow}>Create a POD</button>
+      <button id='service' className='create_menu_component_container_button' onClick={props.handleMenuItemToShow}>Create a Service</button>
+      <button id='deployment' className='create_menu_component_container_button' onClick={props.handleMenuItemToShow}>Create a Deployment</button>
     </div>
   )
 }

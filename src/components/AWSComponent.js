ec680d76a28cc3b98cfaf05ff962e6e0df92e828
Carolyn Harrold <carolyn.harrold@gmail.com>
2019-03-12 21:36:46 -0700

    Component handling

--- src/components/AWSComponent.js
+++ src/components/AWSComponent.js
@@ -14,11 +14,11 @@ const AWSComponent = props => {
         Please input the below details to create your cluster. Once submitted, this phase takes AWS 10-15 minutes.
       </div>
       
-      <div className='aws_cluster_form_container_input'>
+      {/* <div className='aws_cluster_form_container_input'>
         <input id="iamRoleName" placeholder='IAM Role Name' onChange={props.handleChange} value={props.iamRoleName} type="text"/>
         {props.validator.message('Role name', props.iamRoleName, 'required')}
         <h6>Role name can contain alphanumeric and '+=,.@-_' characters. Maximum 64 characters.</h6>
-      </div>
+      </div> */}
 
       <div className='aws_cluster_form_input_field_area'>
         <div className='aws_cluster_form_container_input'>
@@ -39,9 +39,9 @@ const AWSComponent = props => {
           <div className='aws_cluster_form_container_explainer_text'>100 character max, alphanumeric chars, dashes '-' and underscores '_'.</div>
         </div>
 
-        <div className='aws_cluster_form_container_button'>
+        {/* <div className='aws_cluster_form_container_button'>
           <button onClick={props.handleConfigAndMakeNodes} className='buttons'>Submit</button>
-        </div>
+        </div> */}
       </div>  
 
       <div className='aws_cluster_form_container_button_item'>

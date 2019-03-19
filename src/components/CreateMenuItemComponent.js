f6caaefc7fd5718b38172bfd42994472d8a25843
bradenaa <bradenaltstatt@gmail.com>
2019-03-19 13:27:51 -0700

    Began to style the  create cluster component form and created a component for the ActionButton

--- src/components/CreateMenuItemComponent.js
+++ src/components/CreateMenuItemComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ActionButton from './Buttons/ActionButtonComponent';
 import uuid from 'uuid'
 
 const CreateMenuItemComponent = (props) => {
@@ -9,36 +10,38 @@ const CreateMenuItemComponent = (props) => {
     handleCreateDeployment,
     toggleCreateMenuItem,
   } = props;
-  const formItems = Object.entries(props.inputDataToShow).map((arr) => {
+  const componentNameFormatted = menuItemToShow.charAt(0).toUpperCase() + menuItemToShow.slice(1);
+  const formItems = Object.entries(props.inputDataToShow).map((arr, i) => {
     const inputName = arr[0];
     const inputVal = arr[1];
     const id = `${menuItemToShow}_${inputName}`;
     return (
-      <div className='create_menu_item_form_item'>
-        <h4>{menuItemToShow} {inputName}:</h4>
-        <input id={id} key={id} value={inputVal} onChange={handleChange} type="text" />
+      <div className='create_menu_item_component_inputs_item'>
+        {/* <h4>{menuItemToShow} {inputName}:</h4> */}
+        <input id={id} key={i} value={inputVal} placeholder={`${menuItemToShow}-${inputName}`} onChange={handleChange} type="text" />
         {/* TODO: error handling in these forms? this validator seems to force a re-render, which causes issues */}
         {/* {props.validator1.message(`${menuItemToShow} name, ${inputName}, required`)} */}
       </div>
     )
   })
   return (
-    <div onClick={handleCloseFormItem} className="popup">
-      <div className='create_menu_item_component_container'>
-        {/**** TITLE  ****/}
-        <div className='create_menu_item_component_item'>
-          <h2>Create a Deployment</h2>
-        </div>
-        {/**** FORM ****/}
-        <div className='create_menu_item_component_item'>
-          <div className='create_menu_item_form_container'>
+    <div onClick={handleCloseFormItem} className="popup_form">
+      <div className='popup_form_inner'>
+        <div className='create_menu_item_component_container'>
+          {/**** TITLE  ****/}
+          <div className='create_menu_item_component_title'>
+            <h2>Create a {componentNameFormatted}</h2>
+          </div>
+          {/**** FORM ****/}
+          <div className='create_menu_item_component_inputs'>
             {formItems}
           </div>
-        </div>
-        {/**** BUTTONS ****/}
-        <div className='create_menu_item_component_item'>
-          <button onClick={handleCreateDeployment} className="buttons">Create a Deployment</button>
-          <button onClick={toggleCreateMenuItem} className="buttons">Close</button>
+          {/**** BUTTONS ****/}
+          <div className='create_menu_item_component_buttons'>
+            {/* <button onClick={handleCreateDeployment}>Create a {componentNameFormatted}</button> */}
+            <ActionButton clickHandler={handleCreateDeployment} buttonText={`Create`}/>
+            <button onClick={toggleCreateMenuItem}>Close</button>
+          </div>
         </div>
       </div>
     </div>

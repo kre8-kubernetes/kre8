1d92b90e5c2b97a39723ecb74cac0412db5d7513
Carolyn Harrold <carolyn.harrold@gmail.com>
2019-03-22 08:21:09 -0700

    added dyamic naming of worker node functionality to display first portion of UUID on graph

--- src/components/GraphComponents/WorkerNodeComponent.js
+++ src/components/GraphComponents/WorkerNodeComponent.js
@@ -8,18 +8,14 @@ import { Group } from '@vx/group';
 
 
 const WorkerNodeComponent = ({ node, top, left, showNodeInfo }) => {
-  const peach = '#fd9b93';
-  const pink = '#fe6e9e';
-  const blue = '#03c0dc';
-  const green = '#26deb0';
-  const plum = '#71248e';
-  const lightpurple = '#374469';
-  const white = '#ffffff';
-  // const bg = '#272b4d';
-  const medBlue = '#71D7EF';
-
-  // const height = 75;
-  // const width = 60;
+  // const peach = '#fd9b93';
+  // const pink = '#fe6e9e';
+  // const blue = '#03c0dc';
+  // const green = '#26deb0';
+  // const plum = '#71248e';
+  // const lightpurple = '#374469';
+  // const white = '#ffffff';
+  // const medBlue = '#71D7EF';
 
   const bg= '#1D2541';
   const stroke='#5499D9';
@@ -30,21 +26,15 @@ const WorkerNodeComponent = ({ node, top, left, showNodeInfo }) => {
   const centerX = -width / 2;
   const centerY = -height / 2;
 
-  console.log("node.data: ", node.data)
+  // console.log("node.data: ", node.data);
+  // console.log("node.data.status.nodeInfo.systemUUID: ", node.data.status.nodeInfo.systemUUID);
+  // console.log(typeof node.data.status.nodeInfo.systemUUID);
+  // let nodeUUID = node.data.status.nodeInfo.systemUUID.slice(0, 8);
+  // console.log(nodeUUID);
+  //EC2189F9-96F1-88FB-2D04-1CA9852D2802
 
   return (
     <Group top={top} left={left}>
-      {/* <circle
-        r={25}
-        fill={bg}
-        stroke={blue}
-        strokeWidth="1"
-        onClick={() => {
-          showNodeInfo(node);
-          console.log('from circle', node);
-        }}
-      /> */}
-
       <rect
         height={height}
         width={width}
@@ -68,11 +58,23 @@ const WorkerNodeComponent = ({ node, top, left, showNodeInfo }) => {
         fill={'#D7D7D7'}
       >
       <tspan x="0" dy="-.3em">AWS Worker Node</tspan>
-      <tspan x="0" dy="1.5em">EC2260705</tspan>
-      {/* {node.data.name} */}
+      <tspan x="0" dy="1.5em">{ node.data.status.nodeInfo.systemUUID.slice(0, 8) }</tspan>
       </text>
     </Group>
   );
 }
 
 export default WorkerNodeComponent;
+
+
+
+      {/* <circle
+        r={25}
+        fill={bg}
+        stroke={blue}
+        strokeWidth="1"
+        onClick={() => {
+          showNodeInfo(node);
+          console.log('from circle', node);
+        }}
+      /> */}

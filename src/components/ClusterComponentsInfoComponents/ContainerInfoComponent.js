d2f5e6d4eec5de4890e3c5b2e5b6f94da3983362
Carolyn Harrold <carolyn.harrold@gmail.com>
2019-03-31 23:44:58 -0500

    linted ClusterComponentsInfoComponents

--- src/components/ClusterComponentsInfoComponents/ContainerInfoComponent.js
+++ src/components/ClusterComponentsInfoComponents/ContainerInfoComponent.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import InfoBodyComponent from './InfoBodyComponent'
-import ActionButton from '../Buttons/ActionButton'
+import InfoBodyComponent from './InfoBodyComponent';
+import ActionButton from '../Buttons/ActionButton';
 
 const ContainerInfoComponent = (props) => {
-  const { data } = props;
+  const { data, hideNodeInfo } = props;
 
   return (
-    <div className='container_info_component'>
+    <div className="container_info_component">
       <InfoBodyComponent data={data} />
-      <div className='more_info_button_item'>
-        <ActionButton clickHandler={props.hideNodeInfo} buttonText={`Close`} /> 
+      <div className="more_info_button_item">
+        <ActionButton clickHandler={hideNodeInfo} buttonText="Close" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContainerInfoComponent
+export default ContainerInfoComponent;
--- src/components/ClusterComponentsInfoComponents/NodeInfoComponent.js
+++ src/components/ClusterComponentsInfoComponents/NodeInfoComponent.js
@@ -1,47 +1,47 @@
 import React from 'react';
-import { makeInfoItemFromObjectProperties, makeInfoComponentBody } from '../../helperFunctions/renderFunctions'
-import ActionButton from '../Buttons/ActionButton'
+import { makeInfoItemFromObjectProperties, makeInfoComponentBody } from '../../helperFunctions/renderFunctions';
+import ActionButton from '../Buttons/ActionButton';
+
+//TODO delete makeInfoComponentBody?
 
 const NodeInfoComponent = (props) => {
-  const { data } = props;
+  const { data, hideNodeInfo } = props;
 
-  const addresses = data.status.addresses.map((address, i) => {
-    return (
-      <div key={i} className='additional_info_body_item'>
-        <div className='additional_info_body_item_row'>
-          <p>{address.type}</p>
-          <p>{address.address}</p>
-        </div>
+  const addresses = data.status.addresses.map((address, i) => (
+    <div key={i} className="additional_info_body_item">
+      <div className="additional_info_body_item_row">
+        <p>{address.type}</p>
+        <p>{address.address}</p>
       </div>
-    )
-  });
+    </div>
+  ));
 
   const allocatable = makeInfoItemFromObjectProperties(data.status.allocatable, 'Allocatable');
-  const capacity = makeInfoItemFromObjectProperties(data.status.capacity, 'Capacity')
+  const capacity = makeInfoItemFromObjectProperties(data.status.capacity, 'Capacity');
   const nodeInfo = makeInfoItemFromObjectProperties(data.status.nodeInfo, 'NodeInfo');
 
   return (
-    <div className='node_info_component'>
-      <div className='node_info_component_item'>
+    <div className="node_info_component">
+      <div className="node_info_component_item">
         <p>ID</p>
         <p>{data.metadata.uid}</p>
       </div>
-      <div className='node_info_component_item'>
+      <div className="node_info_component_item">
         <p>Component Name</p>
         <p>{data.metadata.name}</p>
       </div>
-      <div className='node_info_component_item'>
+      <div className="node_info_component_item">
         <p>Kind</p>
         <p>{data.kind}</p>
       </div>
-      <div className='node_info_component_item'>
+      <div className="node_info_component_item">
         <p>Created At</p>
         <p>{data.metadata.creationTimestamp}</p>
       </div>
-      <div className='info_component_additional_items'>
+      <div className="info_component_additional_items">
         <p>Addresses -- </p>
-        <div className='additional_info_body_container'>
-          <div className='additional_info_body_item'>
+        <div className="additional_info_body_container">
+          <div className="additional_info_body_item">
             {addresses}
           </div>
         </div>
@@ -49,11 +49,11 @@ const NodeInfoComponent = (props) => {
       {allocatable}
       {capacity}
       {nodeInfo}
-      <div className='more_info_button_item'>
-        <ActionButton clickHandler={props.hideNodeInfo} buttonText={`Close`} /> 
+      <div className="more_info_button_item">
+        <ActionButton clickHandler={hideNodeInfo} buttonText="Close" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NodeInfoComponent
+export default NodeInfoComponent;

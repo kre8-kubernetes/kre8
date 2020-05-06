ebea0fdba33eb6c882db33a6b581ee22e72e60b3
bradenaa <bradenaltstatt@gmail.com>
2020-05-06 16:58:28 -0700

    Updates version and changelog + linting

--- src/client/utils/typeChecks.js
+++ src/client/utils/typeChecks.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-shadow */
+/* eslint-disable arrow-body-style */
 export const isBool = (val) => typeof val === 'boolean';
 
 export const getNested = (obj, ...args) => {
-  return args.reduce((obj, level) => { 
+  return args.reduce((obj, level) => {
     return obj && obj[level];
   }, obj);
 };

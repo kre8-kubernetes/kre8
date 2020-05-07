320c27dc869290a20924b60416846742da1252ae
Braden Altstatt <bradenaltstatt@gmail.com>
2020-05-06 20:02:49 -0700

    Braden/fix nested data (#102)
    Included a util to check for nested data. Some util from lodash or
    underscore could be brought in, but this works for now. There are more
    likely additional bugs like this, but this at lest catches the ones that
    are prevented the dummy data to work.

--- src/client/utils/typeChecks.js
+++ src/client/utils/typeChecks.js
@@ -1,1 +1,9 @@
+/* eslint-disable no-shadow */
+/* eslint-disable arrow-body-style */
 export const isBool = (val) => typeof val === 'boolean';
+
+export const getNested = (obj, ...args) => {
+  return args.reduce((obj, level) => {
+    return obj && obj[level];
+  }, obj);
+};
